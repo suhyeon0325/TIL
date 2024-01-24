9156cac4d596fceb2431eeb35d5596a7b188e306
KimSuHyeon <suhyeonnim0325@gmail.com>
2024-01-24 23:46:35 +0900

    Update and rename 2401224_Joining_Data.py to 2401224_Joining_Data_with_pandas.py

--- Personal/2401224_Joining_Data_with_pandas.py
+++ Personal/2401224_Joining_Data_with_pandas.py
@@ -185,3 +185,169 @@ titles_diff = orig_seq[['title_org','title_seq','diff']]
 
 # Print the first rows of the sorted titles_diff
 print(titles_diff.sort_values('diff', ascending=False).head())
+
+# 3.1. Advanced Merging and Concatenating - Filtering joins
+# Merge employees and top_cust
+empl_cust = employees.merge(top_cust, on='srid', 
+                                 how='left', indicator=True)
+
+# Select the srid column where _merge is left_only
+srid_list = empl_cust.loc[empl_cust['_merge'] == 'left_only', 'srid']
+
+# Get employees not working with top customers
+print(employees[employees['srid'].isin(srid_list)])
+
+# Merge the non_mus_tck and top_invoices tables on tid
+tracks_invoices = non_mus_tcks.merge(top_invoices, on='tid')
+
+# Use .isin() to subset non_mus_tcks to rows with tid in tracks_invoices
+top_tracks = non_mus_tcks[non_mus_tcks['tid'].isin(tracks_invoices['tid'])]
+
+# Group the top_tracks by gid and count the tid rows
+cnt_by_gid = top_tracks.groupby(['gid'], as_index=False).agg({'tid':'count'})
+
+# Merge the genres table to cnt_by_gid on gid and print
+print(cnt_by_gid.merge(genres, on='gid'))
+
+# 3.2. Advanced Merging and Concatenating - Concatenate DataFrames together vertically
+# Concatenate the tracks
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tracks so the index goes from 0 to n-1
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               ignore_index=True,
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tracks, show only columns names that are in all tables
+tracks_from_albums = pd.concat([tracks_master, tracks_ride, tracks_st],
+                               join='inner',
+                               sort=True)
+print(tracks_from_albums)
+
+# Concatenate the tables and add keys
+inv_jul_thr_sep = pd.concat([inv_jul, inv_aug, inv_sep], 
+                            keys=['7Jul', '8Aug', '9Sep'])
+
+# Group the invoices by the index keys and find avg of the total column
+avg_inv_by_month = inv_jul_thr_sep.groupby(level=0).agg({'total':'mean'})
+
+# Bar plot of avg_inv_by_month
+avg_inv_by_month.plot(kind='bar')
+plt.show()
+
+# 3.3. Advanced Merging and Concatenating - Verifying integrity
+# Concatenate the classic tables vertically
+classic_18_19 = pd.concat([classic_18, classic_19], ignore_index=True)
+
+# Concatenate the pop tables vertically
+pop_18_19 = pd.concat([pop_18, pop_19], ignore_index=True)
+
+# Merge classic_18_19 with pop_18_19
+classic_pop = classic_18_19.merge(pop_18_19, on='tid')
+
+# Using .isin(), filter classic_18_19 rows where tid is in classic_pop
+popular_classic = classic_18_19[classic_18_19['tid'].isin(classic_pop['tid'])]
+
+# Print popular chart
+print(popular_classic)
+
+# 4.1. Merging Ordered and Time-Series Data - Using merge_ordered()
+# Use merge_ordered() to merge gdp and sp500, interpolate missing value
+gdp_sp500 = pd.merge_ordered(gdp, sp500, left_on='year', right_on='date', 
+                             how='left',  fill_method='ffill')
+
+# Subset the gdp and returns columns
+gdp_returns = gdp_sp500[['gdp', 'returns']]
+
+# Print gdp_returns correlation
+print (gdp_returns.corr())
+
+# Use merge_ordered() to merge inflation, unemployment with inner join
+inflation_unemploy = pd.merge_ordered(inflation, unemployment, on='date', how='inner')
+
+# Print inflation_unemploy 
+print(inflation_unemploy)
+
+# Plot a scatter plot of unemployment_rate vs cpi of inflation_unemploy
+inflation_unemploy.plot(kind='scatter', x = 'unemployment_rate', y='cpi')
+plt.show()
+
+# Merge gdp and pop on country and date with fill
+date_ctry = pd.merge_ordered(gdp, pop, on=['country', 'date'], fill_method='ffill')
+
+# Print date_ctry
+print(date_ctry)
+
+# 4.2. Merging Ordered and Time-Series Data - Using merge_asof()
+# Use merge_asof() to merge jpm and wells
+jpm_wells = pd.merge_asof(jpm, wells, on='date_time', 
+                          suffixes=('', '_wells'), direction='nearest')
+
+# Use merge_asof() to merge jpm_wells and bac
+jpm_wells_bac = pd.merge_asof(jpm_wells, bac, on='date_time', 
+                              suffixes=('_jpm', '_bac'), direction='nearest')
+
+# Compute price diff
+price_diffs = jpm_wells_bac.diff()
+
+# Plot the price diff of the close of jpm, wells and bac only
+price_diffs.plot(y=['close_jpm','close_wells','close_bac'])
+plt.show()
+
+# Merge gdp and recession on date using merge_asof()
+gdp_recession = pd.merge_asof(gdp, recession, on='date')
+
+# Create a list based on the row value of gdp_recession['econ_status']
+is_recession = ['r' if s=='recession' else 'g' for s in gdp_recession['econ_status']]
+
+# Plot a bar chart of gdp_recession
+gdp_recession.plot(kind='bar', y='gdp', x='date', color=is_recession, rot=90)
+plt.show()
+
+# 4.3. Merging Ordered and Time-Series Data - Selecting data with .query()
+# Merge gdp and pop on date and country with fill
+gdp_pop = pd.merge_ordered(gdp, pop, on=['country','date'], fill_method='ffill')
+
+# Add a column named gdp_per_capita to gdp_pop that divides the gdp by pop
+gdp_pop['gdp_per_capita'] = gdp_pop['gdp'] / gdp_pop['pop']
+
+# Pivot data so gdp_per_capita, where index is date and columns is country
+gdp_pivot = gdp_pop.pivot_table('gdp_per_capita', 'date', 'country')
+
+# Select dates equal to or greater than 1991-01-01
+recent_gdp_pop = gdp_pivot.query('date >= "1991-01-01"')
+
+# Plot recent_gdp_pop
+recent_gdp_pop.plot(rot=90)
+plt.show()
+
+# 4.4. Merging Ordered and Time-Series Data - Reshaping data with .melt()
+# unpivot everything besides the year column
+ur_tall = ur_wide.melt(id_vars=['year'], var_name=['month'], value_name='unempl_rate')
+
+
+# Create a date column using the month and year columns of ur_tall
+ur_tall['date'] = pd.to_datetime(ur_tall['year'] + '-' + ur_tall['month'])
+
+# Sort ur_tall by date in ascending order
+ur_sorted = ur_tall.sort_values('date')
+
+# Plot the unempl_rate by date
+ur_sorted.plot(x='date', y='unempl_rate')
+plt.show()
+
+# Use melt on ten_yr, unpivot everything besides the metric column
+bond_perc = ten_yr.melt(id_vars='metric', var_name='date', value_name='close')
+
+# Use query on bond_perc to select only the rows where metric=close
+bond_perc_close = bond_perc.query('metric == "close"')
+
+# Merge (ordered) dji and bond_perc_close on date with an inner join
+dow_bond = pd.merge_ordered(dji, bond_perc_close, on='date', how='inner', suffixes=('_dow', '_bond'))
+
+# Plot only the close_dow and close_bond columns
+dow_bond.plot(y=['close_dow', 'close_bond'], x='date', rot=90)
+plt.show()
