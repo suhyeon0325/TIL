b28f1a6323906b9f47772097c906677cfb5e7188
KimSuHyeon <suhyeonnim0325@gmail.com>
2024-01-26 17:34:52 +0900

    Update

--- Personal/240125_EDA_in_Python.py
+++ Personal/240125_EDA_in_Python.py
@@ -58,14 +58,155 @@ sns.barplot(x="continent", y="2021", data=unemployment)
 plt.show()
 
 # 2.1. Getting to Know a Dataset - Addressing missing data
+# Count the number of missing values in each column
+print(planes.isna().sum())
+
+# Find the five percent threshold
+threshold = len(planes) * 0.05
+
+# Create a filter
+cols_to_drop = planes.columns[planes.isna().sum() <= threshold]
+
+# Drop missing values for columns below the threshold
+planes.dropna(subset=cols_to_drop, inplace=True)
+
+print(planes.isna().sum())
+
+# Check the values of the Additional_Info column
+print(planes["Additional_Info"].value_counts())
+
+# Create a box plot of Price by Airline
+sns.boxplot(data=planes, x='Airline', y='Price')
+
+plt.show()
+
+# Calculate median plane ticket prices by Airline
+airline_prices = planes.groupby("Airline")["Price"].median()
+
+print(airline_prices)
+
+# Convert to a dictionary
+prices_dict = airline_prices.to_dict()
+
+# Map the dictionary to missing values of Price by Airline
+planes["Price"] = planes["Price"].fillna(planes["Airline"].map(prices_dict))
+
+# Check for missing values
+print(planes.isna().sum())
+
 # 2.2. Getting to Know a Dataset - Converting and analyzing categorical data
+# Filter the DataFrame for object columns
+non_numeric = planes.select_dtypes("object")
+
+# Loop through columns
+for col in non_numeric.columns:
+  
+# Print the number of unique values
+print(f"Number of unique values in {col} column: ", non_numeric[col].nunique())
+
+# Create a list of categories
+flight_categories = ["Short-haul", "Medium", "Long-haul"]
+
+# Create short_flights
+short_flights = "^0h|^1h|^2h|^3h|^4h"
+
+# Create medium_flights
+medium_flights = "^5h|^6h|^7h|^8h|^9h"
+
+# Create long_flights
+long_flights = "10h|11h|12h|13h|14h|15h|16h"
+
+# Create conditions for values in flight_categories to be created
+conditions = [
+    (planes["Duration"].str.contains(short_flights)),
+    (planes["Duration"].str.contains(medium_flights)),
+    (planes["Duration"].str.contains(long_flights))
+]
+
+# Apply the conditions list to the flight_categories
+planes["Duration_Category"] = np.select(conditions, 
+                                        flight_categories,
+                                        default="Extreme duration")
+
+# Plot the counts of each category
+sns.countplot(data=planes, x="Duration_Category")
+plt.show()
+
 # 2.3. Getting to Know a Dataset - Working with numeric data
+# Preview the column
+print(planes["Duration"].head())
+
+# Remove the string character
+planes["Duration"] = planes["Duration"].str.replace("h", "")
+
+# Convert to float data type
+planes["Duration"] = planes["Duration"].astype(float)
+
+# Plot a histogram
+sns.histplot(data=planes, x="Duration")
+plt.show()
+
+# Price standard deviation by Airline
+planes["airline_price_st_dev"] = planes.groupby("Airline")["Price"].transform(lambda x: x.std())
+
+print(planes[["Airline", "airline_price_st_dev"]].value_counts())
+
+# Median Duration by Airline
+planes["airline_median_duration"] = planes.groupby("Airline")["Duration"].transform(lambda x: x.median())
+
+print(planes[["Airline","airline_median_duration"]].value_counts())
+
+# Mean Price by Destination
+planes["price_destination_mean"] = planes.groupby("Destination")["Price"].transform(lambda x: x.mean())
+
+print(planes[["Destination","price_destination_mean"]].value_counts())
+
 # 2.4. Getting to Know a Dataset - Handling outliers
+# Find the 75th and 25th percentiles
+price_seventy_fifth = planes["Price"].quantile(0.75)
+price_twenty_fifth = planes["Price"].quantile(0.25)
+
+# Calculate iqr
+prices_iqr = price_seventy_fifth - price_twenty_fifth
+
+# Calculate the thresholds
+upper = price_seventy_fifth + (1.5 * prices_iqr)
+lower = price_twenty_fifth - (1.5 * prices_iqr)
+
+# Subset the data
+planes = planes[(planes["Price"] > lower) & (planes["Price"] < upper)]
+
+print(planes["Price"].describe())
 
 # 3.1. Relationships in Data - Patterns over time
-# 3.2. Relationships in Data - Correlation
-# 3.3. Relationships in Data - Factor relationships and distributions
+# Import divorce.csv, parsing the appropriate columns as dates in the import
+divorce = pd.read_csv("divorce.csv", parse_dates=['divorce_date', 'dob_man', 'dob_woman', 'marriage_date'])
+print(divorce.dtypes)
 
+# Define the marriage_year column
+divorce["marriage_year"] = divorce["marriage_date"].dt.year
+
+# Create a line plot showing the average number of kids by year
+sns.lineplot(data=divorce, x='marriage_year', y='num_kids')
+plt.show()
+
+# 3.2. Relationships in Data - Correlation
+# Create the scatterplot
+sns.scatterplot(x='marriage_duration', y='num_kids', data=divorce)
+plt.show()
+
+# Create a pairplot for income_woman and marriage_duration
+sns.pairplot(divorce, vars=['income_woman', 'marriage_duration'])
+plt.show()
+
+# 3.3. Relationships in Data - Factor relationships and distributions
+# Create the scatter plot
+sns.scatterplot(x='woman_age_marriage', y='income_woman', hue='education_woman', data=divorce)
+plt.show()
+
+# Update the KDE plot to show a cumulative distribution function
+sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0, cumulative=True)
+plt.show()
 # 4.1. Turning Exploratory Analysis into Action - Consideration for categorical data
 # 4.2. Turning Exploratory Analysis into Action - Generating new features
 # 4.3. Turning Exploratory Analysis into Action - Generating hypotheses
