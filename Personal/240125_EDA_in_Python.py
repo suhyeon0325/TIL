5cb78d0090377b89976ba1f2661a25fec7e1fae5
KimSuHyeon <suhyeonnim0325@gmail.com>
2024-01-29 00:36:45 +0900

    Update

--- Personal/240125_EDA_in_Python.py
+++ Personal/240125_EDA_in_Python.py
@@ -207,6 +207,65 @@ plt.show()
 # Update the KDE plot to show a cumulative distribution function
 sns.kdeplot(data=divorce, x="marriage_duration", hue="num_kids", cut=0, cumulative=True)
 plt.show()
+
 # 4.1. Turning Exploratory Analysis into Action - Consideration for categorical data
+# Print the relative frequency of Job_Category
+print(salaries["Job_Category"].value_counts(normalize=True))
+
+# Cross-tabulate Company_Size and Experience
+print(pd.crosstab(salaries["Company_Size"], salaries["Experience"]))
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"]))
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"],
+            values=salaries["Salary_USD"], aggfunc='mean'))
+
 # 4.2. Turning Exploratory Analysis into Action - Generating new features
+# Get the month of the response
+salaries["month"] = salaries["date_of_response"].dt.month
+
+# Extract the weekday of the response
+salaries["weekday"] = salaries["date_of_response"].dt.weekday
+
+# Create a heatmap
+sns.heatmap(salaries.corr(), annot=True)
+plt.show()
+
+# Find the 25th percentile
+twenty_fifth = salaries["Salary_USD"].quantile(0.25)
+
+# Save the median
+salaries_median = salaries["Salary_USD"].median()
+
+# Gather the 75th percentile
+seventy_fifth = salaries["Salary_USD"].quantile(0.75)
+print(twenty_fifth, salaries_median, seventy_fifth)
+
+# Create salary labels
+salary_labels = ["entry", "mid", "senior", "exec"]
+
+# Create the salary ranges list
+salary_ranges = [0, twenty_fifth, salaries_median, seventy_fifth, salaries["Salary_USD"].max()]
+
+# Create salary_level
+salaries["salary_level"] = pd.cut(salaries["Salary_USD"],
+                                  bins=salary_ranges,
+                                  labels=salary_labels)
+
+# Plot the count of salary levels at companies of different sizes
+sns.countplot(data=salaries, x="Company_Size", hue="salary_level")
+plt.show()
+
 # 4.3. Turning Exploratory Analysis into Action - Generating hypotheses
+# Filter for employees in the US or GB
+usa_and_gb = salaries[salaries["Employee_Location"].isin(["US", "GB"])]
+
+# Create a barplot of salaries by location
+sns.barplot(data=usa_and_gb, x="Employee_Location", y="Salary_USD")
+plt.show()
+
+# Create a bar plot of salary versus company size, factoring in employment status
+sns.barplot(data=salaries, x="Company_Size", y="Salary_USD", hue="Employment_Status")
+plt.show()
