a2c2fd3a004c84ec21ed5b5474c07af2a3cad304
KimSuHyeon <suhyeonnim0325@gmail.com>
2024-02-01 17:45:58 +0900

    Update 240131_Plotly.py

--- Personal/240131_Plotly.py
+++ Personal/240131_Plotly.py
@@ -205,10 +205,103 @@ fig.update_layout({'annotations': [{
   "text": f"This graph was generated at {timestamp}", 
   "showarrow": False, "x": 0.5, "y": 1.1, "xref": "paper", "yref": "paper"}]})
 fig.show()
+
 # 3.1. Advanced Customization - Subplots
+# Set up the subplots grid
+fig = make_subplots(rows=2, cols=2, 
+                    # Set the subplot titles
+                    subplot_titles=['Tech', 'Professional Services', 'Retail', 'Oil'])
+
+# Add the Tech trace
+fig.add_trace(go.Box(x=df_tech.Revenue, name='', showlegend=False), row=1, col=1)
+# Add the Professional Services trace
+fig.add_trace(go.Box(x=df_prof_serve.Revenue, name='', showlegend=False), row=1, col=2)
+# Add the Retail trace
+fig.add_trace(go.Box(x=df_retail.Revenue, name='', showlegend=False), row=2, col=1)
+# Add the Oil trace
+fig.add_trace(go.Box(x=df_oil.Revenue, name='', showlegend=False), row=2, col=2)
+
+# Add a title (and show)
+fig.update_layout({'title': {'text': 'Box plots of company revenues', 'x': 0.5, 'y': 0.9}})
+fig.show()
+
+# Create the subplots
+fig = make_subplots(rows=3, cols=1, shared_xaxes=True)
+
+# Loop through the industries
+row_num = 1
+for industry in ['Tech', 'Retail', 'Professional Services']:
+    df = revenues[revenues.Industry == industry]
+    # Add a histogram using subsetted df
+    fig.add_trace(go.Histogram(x=df['Revenue'], name=industry),
+    # Position the trace
+    row=row_num, col=1)
+    row_num +=1
+
+# Show the plot
+fig.show()
+
 # 3.2. Advanced Customization - Layering multiple plots
+# Create the base figure
+fig = go.Figure()
+
+# Loop through the species
+for species in ['Adelie', 'Chinstrap', 'Gentoo']:
+  # Add a bar chart trace
+  fig.add_trace(go.Bar(x=islands,
+    # Set the appropriate y subset and name
+    y=penguins_grouped[penguins_grouped.Species == species]['Count'],
+    name=species))
+# Show the figure
+fig.show()
+
+# Create the base figure
+fig = go.Figure()
+
+# Add the bar graph of daily temperatures
+fig.add_trace(
+  go.Bar(x=temp_syd['Date'], y=temp_syd['Temp'], name='Daily Max Temperature'))
+
+# Add the monthly average line graph
+fig.add_trace(
+  go.Scatter(x=temp_syd_avg['Date'], y=temp_syd_avg['Average'], name='Average Monthly Temperature'))
+
+# Show the plot
+fig.show()
+
 # 3.3. Advanced Customization - Time buttons
+# Create the buttons
+date_buttons = [
+{'count': 4, 'label': "4WTD", 'step': "day", 'stepmode': "todate"},
+{'count': 48, 'label': "48HR", 'step': "hour", 'stepmode': "todate"},
+{'count': 1, 'label': "YTD", 'step': "year", 'stepmode': "todate"}]
+
+# Create the basic line chart
+fig = px.line(data_frame=rain, x='Date', y='Rainfall', 
+              title="Rainfall (mm)")
+
+# Add the buttons and show
+fig.update_layout(
+  	{'xaxis':
+    {'rangeselector': {'buttons': date_buttons}}})
+fig.show()
+
+# Create the basic line chart
+fig = px.line(data_frame=tesla, x='Date', y='Open', title="Tesla Opening Stock Prices")
+
+# Create the financial buttons
+fin_buttons = [
+  {'count': 7, 'label': "1WTD", 'step': 'day', 'stepmode': 'todate'},
+  {'count': 6, 'label': "6MTD", 'step': 'month', 'stepmode': 'todate'},
+  {'count': 1, 'label': "YTD", 'step': 'year', 'stepmode': 'todate'}
+]
+
+# Create the date range buttons & show the plot
+fig.update_layout({'xaxis': {'rangeselector': {'buttons': fin_buttons}}})
+fig.show()
 
 # 4.1. Advanced Interactivity - Custom buttons
+
+
 # 4.2. Advanced Interactivity - Dropdowns
 # 4.3. Advanced Interactivity - Sliders
